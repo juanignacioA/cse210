25ffd96aec82fd8cfe5c4b67fa9f5e2d2a2c3ef1
juanignacioA <juanignacioarballo@gmail.com>
2026-03-10 13:11:54 -0300

    exercise2 from week01 0.1

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,7 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
 
         Console.Write("What is your grade percentage? ");
         int gradeP = int.Parse(Console.ReadLine());
