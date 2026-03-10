ea4b8322ab9e868bf6a25c119d9134f8aa5e74ef
juanignacioA <juanignacioarballo@gmail.com>
2026-03-10 11:45:05 -0300

    Exercise1 from week01

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project.");
+
+        int a = 0;
+
+        if (a == 0)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write($"Your name is: {name}");
+            
+
+        }
     }
 }
