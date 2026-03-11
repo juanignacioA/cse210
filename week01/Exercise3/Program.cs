6cbd175a2c83ebfcfe896733d4b94ed2ad39aaf2
juanignacioA <juanignacioarballo@gmail.com>
2026-03-11 10:52:05 -0300

    exercise3 from week01

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guessN = -1;
+
+        while (guessN != magicNumber)
+        {
+            //Console.Write("What is the magic number? ");
+            //int magicNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("What is your guess? ");
+            guessN = int.Parse(Console.ReadLine());
+
+            if ( guessN == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if ( guessN > magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            } 
+        }
+
     }
 }
