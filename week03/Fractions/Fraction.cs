88eafaa00e647f1f7d7ed5518c3576929efdefbe
juanignacioA <juanignacioarballo@gmail.com>
2026-03-17 12:15:02 -0300

    fraction from week03

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,50 @@
+using System.Runtime.CompilerServices;
+
+class Fraction{
+    
+    private int _top;
+    private int _bottom;
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+    public void SetTop( int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottomn()
+    {
+        return _bottom;
+    }    
+    public void SetBottomn(int bottom)
+    {
+        _bottom = bottom;
+    }
+    public string GetFractionString()
+    {
+        string FracTxt = $"{_top}/{_bottom}";
+        return FracTxt;
+    }   
+    
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+}
