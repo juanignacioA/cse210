0dec1446332ccf10b2d2ce9dacdb26f56597a521
juanignacioA <juanignacioarballo@gmail.com>
2026-03-30 09:25:06 -0300

    Homework from week05

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Ignacio", "Math");
+
+        Console.WriteLine(assignment1.GetSummary());
+
+        Assignment assignment2 = new Assignment("Ignacio Silva", "Adition");
+        Console.WriteLine(assignment2.GetSummary());
+
+        MathAssignment assignment3 = new MathAssignment("Jorge Smith", "Natural Numbers", "3", "1, 2, 3");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetHomeworkList());
+
+        WritingAssignment assignment4 = new WritingAssignment("Nathan", "Olimpic Games", "Gold Medals");
+        Console.WriteLine(assignment4.GetSummary());
+        Console.WriteLine(assignment4.GetWritingInformation());
     }
 }
