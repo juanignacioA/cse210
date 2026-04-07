21b0c7ac6509da8a2196ba98b159edb761433027
juanignacioA <juanignacioarballo@gmail.com>
2026-04-07 19:10:45 -0300

    shapes from week06

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 4));
+        shapes.Add(new Rectangle("Blue", 5, 10));
+        shapes.Add(new Circle("Yellow", 3));
+
+        foreach (Shape s in shapes)
+        {
+            Console.WriteLine($"The {s.GetColor()} shape has an area of {s.GetArea():F2}.");
+        }
+        
     }
 }
