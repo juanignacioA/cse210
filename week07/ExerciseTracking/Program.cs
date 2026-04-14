6761cd2239f527c4c9220052fc4f3cb873198965
juanignacioA <juanignacioarballo@gmail.com>
2026-04-14 16:02:39 -0300

    excercisetracking from week07

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace ExerciseTracking
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        static void Main(string[] args)
+        {
+            List<Activity> activities = new List<Activity>();
+
+            activities.Add(new Running("03 Nov 2022", 30, 4.8));
+            activities.Add(new StationaryBicycle("03 Nov 2022", 30, 20.0));
+            activities.Add(new Swimming("03 Nov 2022", 30, 20));
+
+            foreach (Activity activity in activities)
+            {
+                Console.WriteLine(activity.GetSummary());
+            }
+        }
     }
 }
---
+++ week07/ExerciseTracking/StationaryBicycle.cs
@@ -0,0 +1,16 @@
+namespace ExerciseTracking
+{
+    public class StationaryBicycle : Activity
+    {
+        private double _speed;
+
+        public StationaryBicycle(string date, int minutes, double speed) : base(date, minutes)
+        {
+            _speed = speed;
+        }
+
+        public override double GetDistance() => (_speed * GetMinutes()) / 60;
+        public override double GetSpeed() => _speed;
+        public override double GetPace() => 60 / _speed;
+    }
+}
